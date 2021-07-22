e707f64b09fc4ce9a6457ee81df85536c77f887f
Hiroaki Date <mmst3l4@gmail.com>
2021-07-22 23:21:37 +0900

    webpackの設定ファイルを作成

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  entry: './src/index.js',
+
+  // モード値を production に設定すると最適化された状態で、
+  // development に設定するとソースマップ有効でJSファイルが出力される
+  mode: 'development',
+
+  output: {
+    path: `${__dirname}/dist`,
+    filename: 'main.js',
+  }
+};
