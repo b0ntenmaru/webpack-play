1a1d50e8dfda5c1241e9c7bb18a726a00d844dd1
Hiroaki Date <mmst3l4@gmail.com>
2021-07-23 08:35:23 +0900

    既存のjsファイルをtsに変更し、tsをコンパイルするための設定を追記

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,24 @@
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/main.ts',
 
   // モード値を production に設定すると最適化された状態で、
   // development に設定するとソースマップ有効でJSファイルが出力される
   mode: 'development',
+
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+      }
+    ],
+  },
+
+  resolve: {
+    extensions: [
+      '.ts', '.js'
+    ],
+  },
 
   output: {
     path: `${__dirname}/dist`,
