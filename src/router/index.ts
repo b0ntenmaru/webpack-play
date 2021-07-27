c75bb6e3e09205b1b99d0287dcd9804fc336558b
Hiroaki Date <mmst3l4@gmail.com>
2021-07-28 01:31:00 +0900

    不要となったファイルを削除

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,16 +1,10 @@
 import Vue from 'vue';
 import VueRouter, { RouteConfig } from 'vue-router';
-import Home from '@/container/HomeContainer.vue';
 import CalendarContainer from '@/container/CalendarContainer.vue';
 
 Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home,
-  },
   {
     path: '/google_calendar_clone',
     name: 'GoogleCalendarClone',
