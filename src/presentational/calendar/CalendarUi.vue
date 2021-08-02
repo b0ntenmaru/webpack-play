3da712cd2769b0b287990f38f0a937276002b86c
Hiroaki Date <mmst3l4@gmail.com>
2021-08-03 03:17:28 +0900

    date pickerで指定した月のカレンダーを表示するロジックを実装

--- src/presentational/calendar/CalendarUi.vue
+++ src/presentational/calendar/CalendarUi.vue
@@ -1,11 +1,17 @@
 <template>
   <div class="calendar-ui">
-    <DateTable :dates="dates" />
+    <div class="sidebar">
+      <input type="date" @change="e => handleChangeDate(e.target.value)" />
+    </div>
+
+    <div class="main-contents">
+      <DateTable :dates="datesOfTargetMonth" />
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from '@vue/composition-api';
+import { defineComponent, computed, reactive } from '@vue/composition-api';
 import DateTable from '@/presentational/calendar/DateTable.vue';
 import { getDateOfTargetMonth } from '@/usecases/calendar/getDateOfMonth';
 
@@ -17,9 +23,34 @@ export default defineComponent({
   },
 
   setup() {
-    const dates = getDateOfTargetMonth(2021, 7);
 
-    return { dates }
+    const today = new Date();
+
+    const date = reactive<{year: number, month: number, day: number}>({
+      year: today.getFullYear(),
+      month: today.getMonth() + 1,
+      day: today.getDate()
+    });
+
+    const datesOfTargetMonth = computed(() => {
+      return getDateOfTargetMonth(date.year, date.month);
+    })
+
+    return { datesOfTargetMonth, date }
+  },
+
+  methods: {
+    handleChangeDate(selectedDate: string) {
+      this.date.year = Number(selectedDate.split('-')[0]);
+      this.date.month = Number(selectedDate.split('-')[1]);
+      this.date.day = Number(selectedDate.split('-')[2]);
+    },
   },
 });
 </script>
+
+<style scoped lang="scss">
+  div.calendar-ui {
+    display: flex;
+  }
+</style>
